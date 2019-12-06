5ee5126bf565b29464d704cc5a40c62e569c0379
quinnc <q.liesch+git@gmail.com>
2019-12-06 11:56:29 -0700

    Initial implementation

---
+++ Advent Of Code 2019/Day4/Calculators/PasswordGuesser.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Day4.Calculators
+{
+    class PasswordGuesser
+    {
+        public void Guess(int bottom, int top)
+        {
+            Parallel.For(bottom, top + 1,
+                currGuess =>
+                {
+                    string guessAsStr = currGuess.ToString();
+                    if (IsIncreasing(guessAsStr) && HasDouble(guessAsStr))
+                    {
+                        matches.Add(currGuess);
+                    }
+                });
+
+        }
+
+        private bool HasDouble(string currGuess)
+        {
+            for (int i = 1; i < currGuess.Length; i++)
+            {
+                if (currGuess[i - 1] == currGuess[i])
+                    return true;
+            }
+
+            return false;
+        }
+
+        private bool IsIncreasing(string currGuess)
+        {
+            for (int i = 1; i < currGuess.Length; i++)
+            {
+                // if the current one is less than the previous, then fail
+                if (currGuess[i] < currGuess[i - 1])
+                    return false;
+            }
+
+            return true;
+        }
+
+        List<int> matches = new List<int>();
+
+        public IReadOnlyList<int> Matches => matches;
+
+        public override string ToString()
+        {
+            string s = "";
+
+            foreach (int m in matches)
+            {
+                s = m.ToString() + Environment.NewLine;
+            }
+
+            return s;
+
+        }
+    }
+}
---
+++ Advent Of Code 2019/Day4/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Day4.Calculators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Day4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PasswordGuesser pg = new PasswordGuesser();
+
+            int lower = 0;
+            bool ok = false;
+
+            ok = Int32.TryParse(tbLower.Text, out lower);
+            if (!ok)
+                MessageBox.Show("Failed to parse lower bounds " + tbLower.Text);
+
+            int upper = 0;
+            bool ok2;
+            ok2 = Int32.TryParse(tbUpper.Text, out upper);
+            if (!ok2)
+                MessageBox.Show("Failed to parse upper bounds " + tbUpper.Text);
+
+            if (!ok || !ok2)
+                return;
+
+            pg.Guess(lower, upper);
+            tbMatches.Text = pg.ToString();
+        }
+    }
+}
