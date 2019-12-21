2c79f135596844bad0232c00c9f0580c72b1d9e3
quinnc <q.liesch+git@gmail.com>
2019-12-21 09:56:19 -0700

    Create day 11 - refactor IntCode to be inherit threaded from the non-threaded version - implement class to track where the robot is and what he has done - start the robot - copy in the UI

---
+++ Advent Of Code 2019/Day11/EmergencyHullPaintingRobot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11
+{
+    class EmergencyHullPaintingRobot
+    {
+
+        private string initcode;
+        private string currentcode;
+        private PaintTracker tracker;
+
+        public void SetCode(string _code)
+        {
+            initcode = _code;
+        }
+
+        public void Paint (int inp)
+        {
+            ParallelCodeRunner intcomp = new ParallelCodeRunner();
+            
+
+            tracker.Output = intcomp.ExternalInput;
+            intcomp.ExternalOutput = tracker.Input;
+            intcomp.ExternalInput.Add("0");
+
+        }
+
+        public int PaintedSquares { get; private set; } = 0;
+    }
+}
---
+++ Advent Of Code 2019/Day11/PaintTracker.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Day11
+{
+    class Location : Tuple<int, int>
+    {
+        public Location (int _x, int _y) : base(_x, _y)
+        {
+        }
+
+        public int x => Item1;
+        public int y => Item2;
+    }
+
+    enum Colour
+    {
+        Black = 0,
+        White = 1
+    }
+
+    enum Direction
+    {
+        Up = 0,
+        Right = 1,
+        Down = 2,
+        Left = 3
+    }
+
+    class PaintTracker
+    {
+
+        protected BlockingCollection<string> externalInput = new BlockingCollection<string>();
+        protected BlockingCollection<string> externalOutput = null;
+
+        protected Dictionary<Location, Colour> visited = new Dictionary<Location, Colour>();
+
+        Location current = new Location(0, 0);
+        Direction facing = Direction.Up;
+
+        // takes in the output from the painting program
+        // -> first output is the colour to paint the current square
+        // -> second output is the directory to turn (then it moves forward one square in that direction) 
+        public BlockingCollection<string> Input => externalInput;
+
+        public BlockingCollection<string> Output
+        {
+            get => externalOutput;
+            set => externalOutput = value;
+        }
+
+        private bool threadedResult = false;
+        private Thread th;
+
+        private bool Run ()
+        {
+            if (Output == null)
+            {
+                Debugger.Break();
+                return false;
+            }
+
+            var val = externalInput.Take();
+            bool isColour = true;
+
+            while (val != "halt")
+            {
+                int valInt = 0;
+                bool ok = false;
+
+                ok = Int32.TryParse(val, out valInt);
+
+                if (!ok)
+                {
+                    Debugger.Break();
+                    return false;
+                }
+
+                if (isColour)
+                {
+                    PaintSquare(valInt);
+                    // next will be a move
+                    isColour = false;
+                }
+                else
+                {
+                    MoveRobot(valInt);
+
+                    // next will be a colour
+                    isColour = true;
+                }
+
+                val = externalInput.Take();
+            }
+
+
+            return true;
+        }
+
+        private void MoveRobot(int turnType)
+        {
+            Turn(turnType);
+
+            current = StepOne();
+        }
+
+        private Location StepOne()
+        {
+            // move the robot in that direction
+            switch (facing)
+            {
+                case Direction.Up:
+                    return new Location(current.x, current.y+1);
+
+                case Direction.Right:
+                    return new Location(current.x+1, current.y);
+
+                case Direction.Down:
+                    return new Location(current.x, current.y - 1);
+
+                case Direction.Left:
+                    return new Location(current.x-1, current.y);
+            }
+
+            Debugger.Break();
+            return current;
+        }
+
+        private void Turn(int turnType)
+        {
+            // update the way the robot is facing
+            switch (facing)
+            {
+                case Direction.Up:
+                {
+                    if (turnType == 0)
+                        facing = Direction.Left;
+                    else
+                        facing = Direction.Right;
+                }
+                break;
+
+                case Direction.Left:
+                {
+                    if (turnType == 1)
+                        facing = Direction.Up;
+                    else
+                        facing = Direction.Down;
+                }
+                break;
+
+                default:
+                {
+                    // for the middle directions
+                    if (turnType == 0)
+                        facing--;
+                    else
+                        facing++;
+
+                }
+                break;
+            }
+        }
+
+        private void PaintSquare(int colourInt)
+        {
+            Colour colour = Colour.Black;
+            if (colourInt == 1)
+                colour = Colour.White;
+
+            if (!visited.ContainsKey(current))
+            {
+                if (colour == Colour.White)
+                {
+                    visited[current] = colour;
+                }
+                // else do nothing
+            }
+            else
+            {
+                // we already changed the colour once so we don't count repaints, but track current colour anyway
+                visited[current] = colour;
+            }
+
+        }
+
+        private static void Threadify(object inst)
+        {
+            if (inst is PaintTracker crInst)
+            {
+                crInst.threadedResult = crInst.Run();
+            }
+            else
+            {
+                Debugger.Break();
+            }
+        }
+
+        public void RunInThread()
+        {
+
+            th = new Thread(Threadify);
+            th.Start(this);
+
+            Thread.Sleep(0);
+        }
+
+        public bool ThreadedResult()
+        {
+            th.Join();
+            return this.threadedResult;
+        }
+
+    }
+}
